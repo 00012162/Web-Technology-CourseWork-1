814f5d2e64e3aaa6b16269d3c69cd49a3657cafa
00012162 <wiut00012162@gmail.com>
2021-12-08 06:02:18 +0500

    9th commit

--- js/script.js
+++ js/script.js
@@ -10,19 +10,45 @@ const menuCart = document.querySelector("#menu__cart");
 window.addEventListener('load', () => {
     let sections = document.querySelectorAll(".section");
     sections.forEach((section) => {
+    // for Search Icon
+        // creating new elements
         let inputBox = document.createElement("div") ;
         let inputBoxSearch = document.createElement("input");
         let span = document.createElement("span");
 
+        // adding styles to new elements
         inputBox.classList.add("inputBox_search");
         inputBox.setAttribute('id', 'inputBox')
         inputBoxSearch.classList.add("inputBoxSearch");
         span.classList.add("fas", "fa-search", "inputBox__span");
 
+        // connecting elements each other
         inputBox.appendChild(inputBoxSearch);
         inputBox.appendChild(span);
+        section.appendChild(inputBox);        
+    
+    // for Cart Icon
+        //creating element
+        let mainCart = document.createElement('div');
 
-    section.appendChild(inputBox);
+        // adding style to element
+        mainCart.classList.add('main__cart');
+        mainCart.setAttribute('id', 'menu__cart');
+
+        // main content
+        let text = "";
+        for (let i = 1; i < 5; i++){
+        text += `<div class="main__cart-box">
+        <img src="./images/cart-item-${i}.png" alt="cart-item-${i}">
+        <div class="box__content">
+            <h3>Cart Item 0${i}</h3>
+            <p class="price">$15.99/-</p>
+        </div>
+        <span class="fas fa-times"></span>
+        </div>`
+        mainCart.innerHTML = text;
+        section.appendChild(mainCart);
+        }
     })
 });
 
