92f312be23048ce9a1496e0e16c3aadc64524e86
00012162 <wiut00012162@gmail.com>
2021-12-09 02:18:28 +0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Coffee-Shop
+This website is created for CourseWork 1 of WebTechnology module.
+
+The title of this webste is Coffee Shop. It is a website of one shop who sells different kind of coffee. This website includes abundant number of information about this shop. There is main page which allows the user to change the page what they want. In second page (About), user clients know the speciality of the coffee that shop provides. In the menu page, they can book any cofee. In order to book them, it is needed to click "add to cart" utton, that is it. Next page is Products, whereas far from than coffee, clients can book, like and view the product more clearly too. As for the Review page, it includes brief information about the commect coming from the clients. It is much more crucial thing, because new clients are dependence for checking ratings and comments. Obviously, Leaving the name, number and email is crucial part of the website and shop too, because clients may have some recommendations or complaint. For this convenience, creating one page for contacting effect more effeciently. In last page whhiich is Blog, some pictures ca be kept, for example prepared coffee, of course. As we know, there are different kind of coffees with lots of views. 
+
+In generall, this website is created for laptop or PC users, but it is also responsive which creates comfort for phone and tab users. There is input for searching the data they want and cart for purchasing too. As for the navigation, for larger screens it is situated on the top like navigation bar, while for phone it will be available in different page after clicking burger icon. After choosing the page, mobile navigation will be closed automatically. One more tool is that, lets say cart is avaialble to see and we want to search some information on the web site, in this case after opening the input, cart will be closed automatiaclly for better visuality. 
+
+
+
+p/s :
+This website does not provide any back-end functionality, that is why some function like searching, sign in, sign up would not work.
+
+
+
+Link for github repository:
+
+    https://github.com/00012162/Web-Technology-CourseWork-1.git
--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ window.addEventListener('DOMContentLoaded', () => {
             </div>
             <span class="fas fa-times"></span>
             </div>`
-            mainCart.innerHTML = text;
+            mainCart.innerHTML = text + `<a href="#" class="btn">Check Out Now</a>`;
             section.appendChild(mainCart);
             }
             // Cart Btn
@@ -105,4 +105,57 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         })
     });
+    const form = document.getElementById('form');
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const number = document.getElementById('number');
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        
+        checkInputs();
+    });
+
+    function checkInputs() {
+        // trim to remove the whitespaces
+        const nameValue = name.value.trim();
+        const emailValue = email.value.trim();
+        const numberValue = number.value.trim();
+        
+        if(nameValue === '') {
+            setErrorFor(name, 'Username cannot be blank');
+        } else {
+            setSuccessFor(name);
+        }
+        
+        if(emailValue === '') {
+            setErrorFor(email, 'Email cannot be blank');
+        } else if (!isEmail(emailValue)) {
+            setErrorFor(email, 'Not a valid email');
+        } else {
+            setSuccessFor(email);
+        }
+
+        if (numberValue === '') {
+            setErrorFor(number, 'Number cannot be blank');
+        }else{
+            setSuccessFor(number)
+        }
+
+        function setErrorFor(input, message) {
+            const inputBox = input.parentElement;
+            const small = inputBox.querySelector('small');
+            inputBox.className = 'inputBox error';
+            small.innerText = message;
+        }
+
+        function setSuccessFor(input) {
+            const inputBox = input.parentElement;
+            inputBox.className = 'inputBox success';
+        }
+            
+        function isEmail(email) {
+            return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+        }
+    }
 })
